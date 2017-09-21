4f7314ca0c0d14bc641600112029f689583bb61f
root <schmittjoaopedro@gmail.com>
2017-09-20 21:55:25 -0300

    fixed tsp aco errors

--- src/main/java/schmitt/joao/App.java
+++ src/main/java/schmitt/joao/App.java
@@ -20,21 +20,21 @@ import java.util.Comparator;
  */
 public class App {
 
-    public static String tspFile = "/home/joao/projects/master-degree/aco-tsp-algorithm/tsp/eil51.tsp";
+    public static String tspFile = "/home/joao/projects/master-degree/aco-tsp-algorithm/tsp/att532.tsp";
 
     public static void main(String[] args) {
         initializeData();
-        for(int t = 0; t < 1000; t++) {
+        for(int t = 0; t < 300; t++) {
             constructSolution();
             //localSearch();
-            updateStatistics();
+            updateStatistics(t);
             updatePheromoneTrails();
         }
     }
 
     public static void initializeData() {
         double[][] distances = Reader.getDistances(tspFile);
-        ProblemData.p = 0.5;
+        ProblemData.rho = 0.5;
         ProblemData.alpha = 1.0;
         ProblemData.beta = 2.0;
         ProblemData.n = distances.length;
@@ -46,87 +46,40 @@ public class App {
                 ProblemData.dist[i][j] = (int) distances[i][j];
             }
         }
-        ProblemData.nn_list = new int[ProblemData.n][ProblemData.nn];
-        for(int i = 0; i < ProblemData.dist.length; i++) {
-            Integer[] index = new Integer[ProblemData.dist[i].length];
-            Integer[] data = new Integer[ProblemData.n];
-            for(int j = 0; j < ProblemData.dist[i].length; j++) {
-                index[j] = j;
-                data[j] = ProblemData.dist[i][j];
-            }
-            data[i] = Collections.max(Arrays.asList(data));
-            Arrays.sort(index, new Comparator<Integer>() {
-                public int compare(final Integer o1, final Integer o2) {
-                    return Integer.compare(data[o1], data[o2]);
-                }
-            });
-            for(int r = 0; r < ProblemData.nn; r++) {
-                ProblemData.nn_list[i][r] = index[r];
-            }
-        }
-        ProblemData.pheromone = new double[ProblemData.n][ProblemData.n];
-        int count = 0;
-        int current = 0;
-        double total = 0;
-        int[] visited = new int[ProblemData.n];
-        while(count < ProblemData.n) {
-            for(int j = 0; j < ProblemData.nn_list[current].length; j++) {
-                if(visited[ProblemData.nn_list[current][j]] != 1) {
-                    current = ProblemData.nn_list[count][j];
-                    visited[current] = 1;
-                    total += ProblemData.dist[count][current];
-                    break;
-                }
-            }
-            count++;
-        }
-        for(int i = 0; i < ProblemData.pheromone.length; i++) {
-            for(int j = i; j < ProblemData.pheromone[i].length; j++) {
-                ProblemData.pheromone[i][j] = ProblemData.m / total;
-                ProblemData.pheromone[j][i] = ProblemData.m / total;
-            }
-        }
-        ProblemData.choice_info = new double[ProblemData.n][ProblemData.n];
-        computeChoiceInformation();
+
+        ProblemData.generate_nn_list();
         ProblemData.ants = new Ant[ProblemData.m];
         for(int m = 0; m < ProblemData.m; m++) {
             ProblemData.ants[m] = new Ant();
             ProblemData.ants[m].tour = new int[ProblemData.n + 1];
-            ProblemData.ants[m].visited = new int[ProblemData.n];
+            ProblemData.ants[m].visited = new boolean[ProblemData.n];
         }
+        ProblemData.pheromone = new double[ProblemData.n][ProblemData.n];
+        ProblemData.choice_info = new double[ProblemData.n][ProblemData.n];
+        Ant.init_trial(ProblemData.ants[0]);
+        ProblemData.init_pheromone(Ant.trail0);
+        computeChoiceInformation();
+
         System.out.println("Data initialized!");
     }
 
     public static void constructSolution() {
-        for(int k = 0; k < ProblemData.m; k++) {
-            for(int i = 0; i < ProblemData.n; i++) {
-                ProblemData.ants[k].visited[i] = 0;
-            }
-        }
         int step = 0;
         for(int k = 0; k < ProblemData.m; k++) {
-            int r = (int) (Math.random() * ProblemData.n);
-            ProblemData.ants[k].tour[step] = r;
-            ProblemData.ants[k].visited[r] = 1;
+            ProblemData.ants[k].clear_ant_visited();
+            ProblemData.ants[k].place_ant_start(step);
         }
-        while(step < ProblemData.n) {
+        while(step < ProblemData.n - 1) {
             step++;
             for(int k = 0; k < ProblemData.m; k++) {
                 neighborListASDecisionRule(k, step);
             }
         }
+        step = ProblemData.n;
         for(int k = 0; k < ProblemData.m; k++) {
             ProblemData.ants[k].tour[ProblemData.n] = ProblemData.ants[k].tour[0];
-            ProblemData.ants[k].tour_length = computeTourLength(k);
+            ProblemData.ants[k].tour_length = ProblemData.ants[k].computeTourLength();
         }
-    }
-
-    public static int computeTourLength(int k) {
-        int tour_length = 0;
-        for(int i = 0; i < ProblemData.n; i++) {
-            tour_length += ProblemData.dist[ProblemData.ants[k].tour[i]][ProblemData.ants[k].tour[i + 1]];
-        }
-        return tour_length;
     }
 
     public static void ASDecisionRule(int k, int i) {
@@ -134,7 +87,7 @@ public class App {
         double sum_probabilities = 0;
         double[] selection_probability = new double[ProblemData.n];
         for(int j = 0; j < ProblemData.n; j++) {
-            if(ProblemData.ants[k].visited[j] == 1) {
+            if(ProblemData.ants[k].visited[j]) {
                 selection_probability[j] = 0.0;
             } else {
                 selection_probability[j] = ProblemData.choice_info[c][j];
@@ -149,33 +102,38 @@ public class App {
             p += selection_probability[j];
         }
         ProblemData.ants[k].tour[i] = j;
-        ProblemData.ants[k].visited[j] = 1;
+        ProblemData.ants[k].visited[j] = true;
     }
 
     public static void neighborListASDecisionRule(int k, int i) {
-        int c = ProblemData.ants[k].tour[i - 1];
+        int current_city = ProblemData.ants[k].tour[i - 1];
         double sum_probabilities = 0.0;
         double[] selection_probability = new double[ProblemData.nn];
         for(int j = 0; j < ProblemData.nn; j++) {
-            if(ProblemData.ants[k].visited[ProblemData.nn_list[c][j]] == 1) {
+            if(ProblemData.ants[k].visited[ProblemData.nn_list[current_city][j]]) {
                 selection_probability[j] = 0.0;
             } else {
-                selection_probability[j] = ProblemData.choice_info[c][ProblemData.nn_list[c][j]];
+                assert (ProblemData.nn_list[current_city][i] >= 0 && ProblemData.nn_list[current_city][i] < ProblemData.n);
+                selection_probability[j] = ProblemData.choice_info[current_city][ProblemData.nn_list[current_city][j]];
                 sum_probabilities += selection_probability[j];
             }
         }
-        if(sum_probabilities == 0) {
-            chooseBestNext(k, i);
+        if(sum_probabilities <= 0) {
+            Ant.choose_best_next(ProblemData.ants[k], i);
         } else {
             double r = Math.random() * sum_probabilities;
             int j = 0;
             double p = selection_probability[j];
-            while(p < r) {
+            while(p <= r) {
                 j++;
                 p += selection_probability[j];
             }
-            ProblemData.ants[k].tour[i] = ProblemData.nn_list[c][j];
-            ProblemData.ants[k].visited[ProblemData.nn_list[c][j]] = 1;
+            if(j == ProblemData.nn) {
+                ProblemData.ants[k].neighbour_choose_best_next(i);
+                return;
+            }
+            ProblemData.ants[k].tour[i] = ProblemData.nn_list[current_city][j];
+            ProblemData.ants[k].visited[ProblemData.nn_list[current_city][j]] = true;
         }
     }
 
@@ -184,13 +142,13 @@ public class App {
         int nc = 0;
         int c = ProblemData.ants[k].tour[i - 1];
         for(int j = 0; j < ProblemData.n; j++) {
-            if(ProblemData.ants[k].visited[j] == 0 && ProblemData.choice_info[c][j] > v) {
+            if(!ProblemData.ants[k].visited[j] && ProblemData.choice_info[c][j] > v) {
                 nc = j;
                 v = ProblemData.choice_info[c][j];
             }
         }
         ProblemData.ants[k].tour[i] = nc;
-        ProblemData.ants[k].visited[nc] = 1;
+        ProblemData.ants[k].visited[nc] = true;
     }
 
     public static void updatePheromoneTrails() {
@@ -204,14 +162,14 @@ public class App {
     public static void evaporate() {
         for(int i = 0; i < ProblemData.n; i++) {
             for(int j = i; j < ProblemData.n; j++) {
-                ProblemData.pheromone[i][j] = (1 - ProblemData.p) * ProblemData.pheromone[i][j];
+                ProblemData.pheromone[i][j] = (1 - ProblemData.rho) * ProblemData.pheromone[i][j];
                 ProblemData.pheromone[j][i] = ProblemData.pheromone[i][j];
             }
         }
     }
 
     public static void depositPheromone(int k) {
-        double delta_tau = 1.0 / ProblemData.ants[k].tour_length;
+        double delta_tau = 1.0 / (double) ProblemData.ants[k].tour_length;
         for(int i = 0; i < ProblemData.n; i++) {
             int j = ProblemData.ants[k].tour[i];
             int l = ProblemData.ants[k].tour[i + 1];
@@ -221,26 +179,26 @@ public class App {
     }
 
     public static void computeChoiceInformation() {
-        for(int i = 0; i < ProblemData.choice_info.length; i++) {
-            double denominator = 0.0;
-            for(int j = 0; j < ProblemData.choice_info[i].length; j++) {
-                if(i != j)
-                    denominator += Math.pow(ProblemData.pheromone[i][j], ProblemData.alpha) + Math.pow(1.0 / ProblemData.dist[i][j], ProblemData.beta);
-            }
-            for(int j = 0; j < ProblemData.choice_info[i].length; j++) {
-                if(i != j) {
-                    double numerator = Math.pow(ProblemData.pheromone[i][j], ProblemData.alpha) + Math.pow(1.0 / ProblemData.dist[i][j], ProblemData.beta);
-                    ProblemData.choice_info[i][j] = numerator / denominator;
-                }
+        for(int i = 0; i < ProblemData.n; i++) {
+            for(int j = 0; j < i; j++) {
+                double heuristic = (1.0 / ((double) ProblemData.dist[i][j] + 0.1));
+                ProblemData.choice_info[i][j] = Math.pow(ProblemData.pheromone[i][j], ProblemData.alpha) * Math.pow(heuristic, ProblemData.beta);
+                ProblemData.choice_info[j][i] = ProblemData.choice_info[i][j];
             }
         }
     }
 
-    public static void updateStatistics() {
+    public static void updateStatistics(int t) {
         int best_tour = 0;
+        int worst_tour = 0;
+        double mean = ProblemData.ants[best_tour].tour_length;
         for(int k = 1; k < ProblemData.m; k++) {
+            mean += ProblemData.ants[k].tour_length;
             if(ProblemData.ants[k].tour_length < ProblemData.ants[best_tour].tour_length) {
                 best_tour = k;
+            }
+            if(ProblemData.ants[k].tour_length > ProblemData.ants[best_tour].tour_length) {
+                worst_tour = k;
             }
         }
         if(ProblemData.ants[best_tour].tour_length < Statistics.route_length) {
@@ -251,7 +209,12 @@ public class App {
                 tour += "->" + ProblemData.ants[best_tour].tour[i];
             }
             tour += "]";
-            System.out.println("Length = " + Statistics.route_length + "\t" + " Tour = " + tour);
+            System.out.println(t +
+                    " -> Best  = " + Statistics.route_length + "\t" +
+                    " -> Mean  = " + (mean / ProblemData.m) + "\t" +
+                    " -> Worst = " + ProblemData.ants[worst_tour].tour_length + "\t" +
+                    " Tour = " + tour);
+            //System.out.println(t + "," + Statistics.route_length);
         }
     }
 
--- src/main/java/schmitt/joao/ProblemData.java
+++ src/main/java/schmitt/joao/ProblemData.java
@@ -1,5 +1,9 @@
 package schmitt.joao;
 
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+
 public class ProblemData {
 
     //Number of points
@@ -27,7 +31,7 @@ public class ProblemData {
     public static Ant[] ants;
 
     //Pheromone evaporation
-    public static double p;
+    public static double rho;
 
     //Pheromone influence
     public static double alpha;
@@ -35,4 +39,37 @@ public class ProblemData {
     //Heuristic influence
     public static double beta;
 
+    //Generate nearest neighbor list
+    public static void generate_nn_list() {
+        ProblemData.nn_list = new int[ProblemData.n][ProblemData.nn];
+        for(int i = 0; i < ProblemData.dist.length; i++) {
+            Integer[] index = new Integer[ProblemData.dist[i].length];
+            Integer[] data = new Integer[ProblemData.n];
+            for(int j = 0; j < ProblemData.dist[i].length; j++) {
+                index[j] = j;
+                data[j] = ProblemData.dist[i][j];
+            }
+            data[i] = Collections.max(Arrays.asList(data));
+            Arrays.sort(index, new Comparator<Integer>() {
+                public int compare(final Integer o1, final Integer o2) {
+                    return Integer.compare(data[o1], data[o2]);
+                }
+            });
+            for(int r = 0; r < ProblemData.nn; r++) {
+                ProblemData.nn_list[i][r] = index[r];
+            }
+        }
+    }
+
+    //Generate initial pheromone
+    public static void init_pheromone(double initialTrial) {
+        for(int i = 0; i < ProblemData.pheromone.length; i++) {
+            for(int j = i; j < ProblemData.pheromone[i].length; j++) {
+                ProblemData.pheromone[i][j] = initialTrial;
+                ProblemData.pheromone[j][i] = initialTrial;
+                ProblemData.choice_info[i][j] = initialTrial;
+                ProblemData.choice_info[j][i] = initialTrial;
+            }
+        }
+    }
 }

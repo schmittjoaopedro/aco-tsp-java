04a08785366d7805fc58718ac52c2e11907dfca6
João Pedro Schmitt <schmittjoaopedro@gmail.com>
2017-12-06 18:36:17 -0200

    # Fixed execution

--- src/main/java/schmitt/joao/aco/Program.java
+++ src/main/java/schmitt/joao/aco/Program.java
@@ -1,5 +1,8 @@
 package schmitt.joao.aco;
 
+import java.io.File;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.Arrays;
 import java.util.List;
 
@@ -16,9 +19,10 @@ import java.util.List;
  */
 public class Program {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
 
-        String tspPath = "/home/joao/projects/master-degree/aco-tsp-algorithm/tsp/";
+        String tspPath = (new File(".")).getCanonicalPath();
+        tspPath = Paths.get(tspPath, "tsp").toAbsolutePath().toString();
         String tspFiles[] = {"lin318.tsp", "att532.tsp", "eil51.tsp", "pcb1173.tsp", "pr2392.tsp"};
 
         Program app = new Program();
